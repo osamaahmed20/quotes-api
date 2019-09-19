b640b0d73f07a061f5ad7a327ef336d99db76d3c
Osama Ahmed <your_email>
2019-09-18 19:25:07 -0700

    Added more documentation

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 /**
- * A basic REST API that stores and provides access to a set of quotes. Each quote will have the following information associated with it:
+ * A basic REST API that stores, retrieves, and deletes quotes. Each quote will have the following information associated with it:
  * id: a unique number to reference the quote
  * quote: a string containing the quote itself
  * author: a string containing the author's name
  * year: the year the quote was recorded or discovered
  */
 
+// imports
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('quotes.db');
 
+// mounts BodyParser as middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var port = 3000;
 
+// ROUTES
+/* 
+** Acknowledges that request for home page has been recieved.
+*/
 app.get('/', function(request, response){
     response.send('Get request received at /');
 });
@@ -25,6 +32,8 @@ app.get('/', function(request, response){
 ** Returns quotes specified by the year query string. 
 ** If no year specified then return all quotes
 ** Quotes are returned as JSON
+** Ex: home/quotes returns all quotes
+** Ex: home/quotes=1900 return quotes from year 1900
 */
 app.get('/quotes', function(req, res){
     if(req.query.year){
@@ -55,6 +64,7 @@ app.get('/quotes', function(req, res){
 /*
 ** Returns quote specified by id. 
 ** Quotes are returned as JSON
+** Ex: home/quotes/:1 return the quote with id=1
 */
 app.get('/quotes/:id', function(req, res){
     let q = 'SELECT * FROM quotes WHERE rowid = ?';
